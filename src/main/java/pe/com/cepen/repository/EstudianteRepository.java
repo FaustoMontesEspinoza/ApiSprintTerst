6b9cb2dfa6d03af0718ba30e1f9ddd42f681003d
Fausto Montes <faustomontesespinoza@gmail.com>
2023-11-18 12:38:25 -0500

    api

---
+++ src/main/java/pe/com/cepen/repository/EstudianteRepository.java
@@ -0,0 +1,15 @@
+package pe.com.cepen.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pe.com.cepen.entity.EstudianteEntity;
+
+
+public interface EstudianteRepository extends JpaRepository<EstudianteEntity,Long>{
+    @Query("SELECT e FROM EstudianteEntity e WHERE e.esta_estu =true")
+    List<EstudianteEntity> findAllCustom();
+    
+    @Query("SELECT e FROM EstudianteEntity e WHERE usuario.id_usu = :id_usu AND usuario.esta_usu = false  AND e.doc_estu =:doc_estu ")
+    List<EstudianteEntity> getByEstudianDni(Long id_usu ,String doc_estu);
+}
